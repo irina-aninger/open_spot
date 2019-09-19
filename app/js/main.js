7f55ea67c9b97a1a175f45502fa899c768362afa
irina_petrushenko <irina.aninger@gmail.com>
2019-09-19 23:36:53 +0300

    part 6

--- app/js/main.js
+++ app/js/main.js
@@ -13,6 +13,31 @@ window.addEventListener('scroll', function () {
 });
 
 // parallax
-
 let scene = document.getElementById('elipse__wrapper');
 let parallaxInstance = new Parallax(scene);
+
+// accordion
+let acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+    });
+}
+
+// form validation
+
+let form = document.querySelectorAll('.feedback input');
+let btnSubmit = document.querySelector('.feedback input.submit');
+
+console.log(btnSubmit);
+
+btnSubmit.addEventListener('mouseover', function () {
+    console.log(form.value);
+    for (let i=0; i<form.length; i++){
+        console.log(form[i].value)
+        if (form[i].value <= 0) {
+            this.classList.add('error')
+        }
+    }
+});
